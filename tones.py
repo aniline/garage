178b04c5cffe52d47190c2fc33c1fbb0f833fc70
Anil N <anilknyn@yahoo.com>
2013-05-05 14:38:43 +0530

    Accepts grayscale 8bit 50x50 bitmap (raw) as input

--- tones.py
+++ tones.py
@@ -1,77 +1,64 @@
 #!python
-
+#
+# Playing around audio spectrum analyser on an android tablet, I tried making lines by
+# whistling myself dizzy. Then thought of making a program for doing it. This is it.
+#
+# IF you have a grayscale 50x50 pixel raw bitmap (2500 bytes), pass it as argument to this
+# script. It would spew out the audio in 44.1 Khz 16 bit little endian one channel PCM.
+# Save it as raw and use aplay, play or sox. Example command to convert and save as
+# WAV file is :
+#
+# python tones.py /tmp/img.raw | sox -r 44100 -t raw -u -c 1 -2 -- - foo.wav
+#
+# Its not fast enough to play back live (you'll encounter buffer underflows and gaps in audio)
+#
 import math, numpy, sys
 
-def tone_with_sines(freq, time, rate):
+cycle = math.radians(360)
+n_rows = 50
+n_cols = 50
+
+# 50 Bands of 100Hz width each
+freq_base = range(1000, 6000, 100)
+
+# Images are have zero index at top, spectrum analyzer has lower freq at bottom
+freq_base.reverse()
+
+def tone_with_sines(freq, time, rate, vals, angles):
     samples = int(rate * time)
-    i = 0
-    angles = map(lambda x: float(0), freq)
-    cycle = math.radians(360)
-    while i < samples:
-        j = 0
-        while j < len(freq):
-            angles[j] = float(angles[j]) + ((cycle * freq[j])/rate)
-            j = j + 1
-        i = i + 1
-        if (len(freq) > 0):
-            val = int(128 * numpy.mean(map(math.sin, angles))) + 128
-        else:
-            val = 128
-        sys.stdout.write(chr(val))
+    buf = bytearray()
+    if (len(freq) > 0):
+        theta = map(lambda x: cycle * x / rate, freq)
+        denom = 1.0/len(freq)
+        for i in xrange(samples):
+            angles = map(lambda x,y: x+y, angles, theta)
+            val = int(32767 * numpy.mean(map(lambda x, y: y * x, map(math.sin, angles), vals))) + 32768
+            buf = buf + chr(val & 0xff) + chr((val & 0xff00) >> 8)
+        sys.stdout.write(buf)
+    return angles
 
-freq_base = range(1000, 5000, 100)
-img = """
-                                     XXXXXXXXXXXXXXXXXXXXXXXXXX           XXXXXXXXXXXXXXXXXXXXXXXXXXX 
-                                     XXXXXXXXXXXXXXXXXXXXXXXXX              XXXXXXXXXXXXXXXXXXXXXXXXX
-               ########              XXXXXXXXXXXXXXXXXXXXXXX            X     XXXXXXXXXXXXXXXXXXXXXXX
-            ####  ## ####            XXXXXXXXXXXXXXXXXXXXXX                    XXXXXXXXXXXXXXXXXXXXXX
-          ###     ##     ###         XXXXXXXXXXXXXXXXXXXXXX           X        XXXXXXXXXXXXXXXXXXXXXX
-         ##       ##       ##        XXXXXXXXXXXXXXXXXXXXXX   XXX   XXXXXX     XXXXXXXXXXXXXXXXXXXXXX
-        ##        ##        ##       XXXXXXXXXXXXXXXXXXXXXX  X XX   X    XX    XXXXXXXXXXXXXXXXXXXXXX
-        ##       ####       ##       XXXXXXXXXXXXXXXXXXXXXX   X   X XX   X     XXXXXXXXXXXXXXXXXXXXXX
-        ##     #######      ##       XXXXXXXXXXXXXXXXXXXXXX  XXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXXXX
-         ##  ##   ##  ##   ##        XXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXX XX      XXXXXXXXXXXXXXXXXXXXX
-          ###     ##    ####         XXXXXXXXXXXXXXXXXXXXXXX   XXXXXXX  XX   XX  XXXXXXXXXXXXXXXXXXXX
-            ###   ##   ###           XXXXXXXXXXXXXXXXXXXXXX  XX      XXXXXX       XXXXXXXXXXXXXXXXXXX
-              ##########             XXXXXXXXXXXXXXXXXXXXX  XXXXXXXXXXXXXXXX       XXXXXXXXXXXXXXXXXX
-                                     XXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXX
-                                     XXXXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXX   X     XXXXXXXXXXXXXX
-                                     XXXXXXXXXXXXXXXXX     XXXXXXXXXXXXXXXXXXXX   X    XXXXXXXXXXXXXX
-                                     XXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXXXX    X     XXXXXXXXXXXX
-          ######      ######         XXXXXXXXXXXXXXX     XXXXXXXXXXXXXXXXXXXXXXX   XX    XXXXXXXXXXXX
-        ########### ##########       XXXXXXXXXXXXXX     XXXXXXXXXXXXXXXXXXXXXXXX    X     XXXXXXXXXXX
-       ########################      XXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXXXXXXX    X      XXXXXXXXXX
-       ########################      XXXXXXXXXXXX      XXXXXXXXXXXXXXXXXXXXXXXXXX  X       XXXXXXXXXX
-        ######################       XXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXXXXXXXXXX          XXXXXXXXXX
-         ###################         XXXXXXXXXXXX XXXX XXXXXXXXXXXXXXXXXXXXXXXX           XXXXXXXXXXX
-           ###############           XXXXXXXXXXX XXXXXX   XXXXXXXXXXXXXXXXXXXX XX       XXXXXXXXXXXXX
-             ############            XXXXXXXX  XXXXXXXXX    XXXXXXXXXXXXXXXXXX XXXXXXXXXX XXXXXXXXXXX
-                #######              XXXXXXXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXX XXXXXXXXXXXX XXXXXXXXX
-                  ###                XXXXXX XXXXXXXXXXXXXXX    XXXXXXXXXXXXX X XXXXXXXXXXXXXX XXXXXXX
-                   #                 XXXXX  XXXXXXXXXXXXXXXX XXXXXXXXXXXXXX  X XXXXXXXXXXXXXXXX XXXXX
-                                     XXXXX  XXXXXXXXXXXXXXXXX               X  XXXXXXXXXXXX  XXXXXXXX
-                                     XXXXX   XXXXXXXXXXXXXXXX  X            X  XXXXXXXX   XXXXXXXXXXX
-                                     XXXXXXXXXXX      XXXXXX   XXXXXXXXXXXXXX   XXXX   XXXXXXXXXXXXXX
-                                     XXXXXXXXXXXXXXXXXX      XXXXXXXXXXXXXXXXX       XXXXXXXXXXXXXXXX
-"""
+if len(sys.argv) > 1:
+    f = open(sys.argv[1])
+    img = f.read()
+    f.close()
+else:
+    print "Pass a 50x50, 8 bit, grayscale, raw, image data file as argument."
+    sys.exit(1)
 
-# tone_with_sines(freq, 3, 22050)
-ptrn = img.split('\n')
-n_rows = len(ptrn)
-n_cols = max(map(len, ptrn))
-
+# To avoid blips
+angles = map(lambda x: float(0), freq_base)
 i = 0
 while (i<n_cols):
     r = 0
     freq = []
+    vals = []
     while (r < n_rows):
-        row = ptrn[r]
+        row = img[r*n_cols:(r*n_cols + n_cols)]
         if (i<len(row)):
             val = row[i]
         else:
-            val = ' '
-        if val != ' ':
-            freq.append(freq_base[n_rows - r])
+            val = 0
+        vals.append(ord(val)/255.0)
         r = r + 1
-    tone_with_sines(freq, 0.07, 22050)
+    angles = tone_with_sines(freq_base, 0.135, 44100, vals, angles)
     i = i + 1
